4e1ac60fb996fc790e2d788ac29225073888d001
Filipe Marques Hottis <lipehottis@gmail.com>
2023-07-30 15:16:34 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Canvas from "./components/Canvas"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header>
+      <h1>PixelArt</h1>
+    </header>
+    <main>
+      {/* Palette */}
+      <div className="palette">
+        <div className="color-black"></div>
+        <div className="color-white"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      {/* Size */}
+      <div className="size">
+        <input type="number" placeholder="5" />
+        x
+        <input type="number" placeholder="5" />
+        <button>Resize</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {/* Bord */}
+      <Canvas />
+    </main>
     </>
   )
 }
---
+++ src/components/Canvas.tsx
@@ -0,0 +1,13 @@
+function Canvas () {
+  return (
+      <div className="canvas">
+        <div className="0"></div>
+        <div className="1"></div>
+        <div className="2"></div>
+        <div className="3"></div>
+        <div className="4"></div>
+      </div>
+  )
+}
+
+export default Canvas;
