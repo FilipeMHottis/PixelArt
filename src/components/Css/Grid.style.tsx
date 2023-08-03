76e122fdfbf3fd96652e7ec1f7e113313c189a58
Filipe Marques Hottis <lipehottis@gmail.com>
2023-08-03 13:06:30 -0300

    retirando comentario e add padding na grid

--- src/components/Css/Grid.style.tsx
+++ src/components/Css/Grid.style.tsx
@@ -12,8 +12,9 @@ const Grid = styled.div<GridProps>`
   display: grid;
   grid-template-rows: repeat(${props => props.rows}, 1fr);
   grid-template-columns: repeat(${props => props.columns}, 1fr);
-  gap: 4px; /* Ajuste o valor conforme desejar */
+  gap: 4px;
   border: 1px solid #000;
+  padding: 5px;
 `;
 
 export default Grid;
