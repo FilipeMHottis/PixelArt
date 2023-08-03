c1ba8a507c57552f6c62c9dd724e718094cdffa1
Filipe Marques Hottis <lipehottis@gmail.com>
2023-08-03 14:43:51 -0300

    fix: :bug: Agora esta salvando de forma correta no localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -5,51 +5,63 @@ import "./styles/normalize.css"
 import {
   getCanvasDataFromLocalStorage, 
   setCanvasDataToLocalStorage,
+  CanvasData,
 } from "./utils/localStorageFunctions"
-
-const initialCanvasData = getCanvasDataFromLocalStorage() || {
-  colors: ['white', 'black', 'red', 'blue', 'green'],
-  selectedColor: 'black',
-  sizeGrid: {
-    rows: 5,
-    columns: 5,
-  },
-  canvas: Array.from({ length: 5 * 5 }, () => 'white'),
-}
 
 function App() {
   const [size, setSize] = useState({
-    rows: initialCanvasData.sizeGrid.rows,
-    columns: initialCanvasData.sizeGrid.columns,
-  })
+    rows: 5,
+    columns: 5,
+  });
 
-  function handleResizeCanvas(rows:number, columns:number) {
+  const [canvasData, setCanvasData] = useState<CanvasData>(() => {
+    const initialCanvasData = getCanvasDataFromLocalStorage() || {
+      colors: ['white', 'black', 'red', 'blue', 'green'],
+      selectedColor: 'black',
+      sizeGrid: {
+        rows: size.rows,
+        columns: size.columns,
+      },
+      canvas: Array.from({ length: size.rows * size.columns }, () => 'white'),
+    };
+    return initialCanvasData;
+  });
+
+  function handleResizeCanvas(rows: number, columns: number) {
     const newCanvasData = {
-      ...initialCanvasData,
+      ...canvasData,
       sizeGrid: {
         rows,
         columns,
       },
-      canvas: Array.from({ length: 5 * 5 }, () => 'white'),
-    }
+      canvas: Array.from({ length: rows * columns }, (_, index) => {
+        if (index < canvasData.canvas.length) {
+          return canvasData.canvas[index];
+        }
+        return 'white';
+      }),
+    };
 
-    setCanvasDataToLocalStorage(newCanvasData)
+    setCanvasData(newCanvasData);
+    setCanvasDataToLocalStorage(newCanvasData);
   }
+
   function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    e.preventDefault()
-    handleResizeCanvas(size.rows, size.columns)
+    e.preventDefault();
+    handleResizeCanvas(size.rows, size.columns);
   }
+
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target
-    setSize({
-      ...size,
+    const { name, value } = e.target;
+    setSize((prevSize) => ({
+      ...prevSize,
       [name]: Number(value),
-    })
+    }));
   }
-  
+
   useEffect(() => {
-    handleResizeCanvas(size.rows, size.columns)
-  }, [])
+    handleResizeCanvas(size.rows, size.columns);
+  }, []);
 
   return (
     <>
@@ -66,22 +78,24 @@ function App() {
           <input 
             type="number" 
             placeholder="5"
-            onChange={ handleChange }
+            name="rows"
+            onChange={handleChange}
           />
           x
           <input 
             type="number" 
             placeholder="5" 
-            onChange={ handleChange }
+            name="columns"
+            onChange={handleChange}
           />
-          <button onClick={ handleClick }>Resize</button>
+          <button onClick={handleClick}>Resize</button>
         </div>
 
         {/* Bord */}
         <Canvas />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
